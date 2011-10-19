8d54614ad3f9d6d039f5801f9c23dbc3e743e10f
Robey Pointer <robey@twitter.com>
2011-10-19 14:06:47 -0700

    JournalFile and tests.

---
+++ src/main/scala/com/twitter/libkestrel/JournalFile.scala
@@ -0,0 +1,248 @@
+/*
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.libkestrel
+
+import com.twitter.conversions.storage._
+import com.twitter.logging.Logger
+import com.twitter.util._
+import java.io.{File, FileInputStream, IOException}
+import java.nio.{ByteBuffer, ByteOrder}
+import java.nio.channels.FileChannel
+
+case class CorruptedJournalException(lastValidPosition: Long, file: File, message: String) extends IOException(message)
+
+object JournalFile {
+  private[this] val BUFFER_SIZE = 128
+
+  val HEADER_WRITER = 0x27642603
+  val HEADER_READER = 0x263C2603
+
+  // throw an error if any queue item is larger than this:
+  val LARGEST_DATA = 16.megabytes
+
+  private[this] def newBuffer(size: Int) = {
+    val buffer = new Array[Byte](size)
+    val byteBuffer = ByteBuffer.wrap(buffer)
+    byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
+    byteBuffer
+  }
+
+  private[this] val localBuffer = new ThreadLocal[ByteBuffer] {
+    override def initialValue: ByteBuffer = newBuffer(BUFFER_SIZE)
+  }
+
+  def buffer: ByteBuffer = buffer(BUFFER_SIZE)
+
+  // thread-local for buffering journal items
+  def buffer(size: Int): ByteBuffer = {
+    val b = localBuffer.get()
+    if (b.limit < size) {
+      localBuffer.set(newBuffer(size))
+      buffer(size)
+    } else {
+      b.clear()
+      b
+    }
+  }
+
+  def append(file: File, timer: Timer, syncJournal: Duration) = {
+    val journal = new JournalFile(file, timer, syncJournal)
+    journal.openForAppend()
+    journal
+  }
+
+  def createWriter(file: File, timer: Timer, syncJournal: Duration) = {
+    val journal = new JournalFile(file, timer, syncJournal)
+    journal.create(HEADER_WRITER)
+    journal
+  }
+
+  def createReader(file: File, timer: Timer, syncJournal: Duration) = {
+    val journal = new JournalFile(file, timer, syncJournal)
+    journal.create(HEADER_READER)
+    journal
+  }
+
+  def openWriter(file: File, timer: Timer, syncJournal: Duration) = {
+    val journal = new JournalFile(file, timer, syncJournal)
+    if (journal.openForRead() != HEADER_WRITER) throw new IOException("Not a writer journal file")
+    journal
+  }
+
+  def openReader(file: File, timer: Timer, syncJournal: Duration) = {
+    val journal = new JournalFile(file, timer, syncJournal)
+    if (journal.openForRead() != HEADER_READER) throw new IOException("Not a reader journal file")
+    journal
+  }
+
+  // returned from journal replay
+  sealed trait Record
+  object Record {
+    case class Put(item: QueueItem) extends Record
+    case class ReadHead(id: Long) extends Record
+    case class ReadDone(ids: Seq[Long]) extends Record
+  }
+}
+
+class JournalFile(file: File, timer: Timer, syncJournal: Duration) {
+  import JournalFile._
+
+  private[this] val log = Logger.get(getClass)
+
+  private[this] var writer: PeriodicSyncFile = null
+  private[this] var reader: FileChannel = null
+
+  def openForAppend() {
+    writer = new PeriodicSyncFile(file, timer, syncJournal)
+  }
+
+  def openForRead() = {
+    reader = new FileInputStream(file).getChannel
+    val b = buffer
+    b.limit(4)
+    b.order(ByteOrder.BIG_ENDIAN)
+    var x = 0
+    do {
+      x = reader.read(b)
+    } while (b.position < b.limit && x >= 0)
+    if (x < 0) throw new CorruptedJournalException(0, file, "No header in journal file: " + file)
+    b.flip()
+    b.getInt()
+  }
+
+  def create(header: Int) {
+    writer = new PeriodicSyncFile(file, timer, syncJournal)
+    writer.position = 0
+    val b = buffer(4)
+    b.order(ByteOrder.BIG_ENDIAN)
+    b.putInt(header)
+    b.flip()
+    writer.write(b)
+  }
+
+  def close() {
+    if (writer ne null) writer.close()
+    if (reader ne null) reader.close()
+  }
+
+  private[this] val READ_HEAD = 0
+  private[this] val PUT = 8
+  private[this] val READ_DONE = 9
+
+  def put(item: QueueItem): Future[Unit] = {
+    if (item.data.size > LARGEST_DATA.inBytes) {
+      throw new IOException("item too large")
+    }
+    val b = buffer(item.data.size + (7 * 4) + 1)
+    val size = if (item.expireTime.isDefined) 7 else 5
+    b.put((PUT << 4 | size).toByte)
+    b.putInt(item.data.size)
+    b.putLong(item.id)
+    b.putLong(item.addTime.inMilliseconds)
+    item.expireTime.foreach { t => b.putLong(t.inMilliseconds) }
+    b.put(item.data)
+    write(b)
+  }
+
+  def readHead(id: Long): Future[Unit] = {
+    val b = buffer(9)
+    b.put((READ_HEAD << 4 | 2).toByte)
+    b.putLong(id)
+    write(b)
+  }
+
+  def readDone(ids: Seq[Long]): Future[Unit] = {
+    val b = buffer(8 * ids.size + 5)
+    b.put((READ_DONE << 4 | 1).toByte)
+    b.putInt(8 * ids.size)
+    ids.foreach { b.putLong(_) }
+    write(b)
+  }
+
+  private[this] def write(b: ByteBuffer) = {
+    b.flip()
+    writer.write(b)
+  }
+
+  def readNext(): Option[JournalFile.Record] = {
+    val lastPosition = reader.position
+    val b = buffer
+    b.limit(1)
+    var x: Int = 0
+    do {
+      x = reader.read(b)
+    } while (b.position < b.limit && x >= 0)
+
+    if (x < 0) return None
+    val command = (b.get(0) >> 4) & 0xf
+    val headerSize = (b.get(0) & 0xf)
+    b.clear()
+    b.limit(headerSize * 4)
+    do {
+      x = reader.read(b)
+    } while (b.position < b.limit && x >= 0)
+    b.rewind()
+
+    var data: Array[Byte] = null
+    if (command >= 8) {
+      val dataSize = b.getInt()
+      if (dataSize > LARGEST_DATA.inBytes) {
+        throw new CorruptedJournalException(lastPosition, file, "item too large")
+      }
+
+      data = new Array[Byte](dataSize)
+      val dataBuffer = ByteBuffer.wrap(data)
+      do {
+        x = reader.read(dataBuffer)
+      } while (dataBuffer.position < dataBuffer.limit && x >= 0)
+    }
+
+    if (x < 0) throw new CorruptedJournalException(lastPosition, file, "truncated entry")
+
+    Some(command match {
+      case READ_HEAD => {
+        if (b.limit < 8) {
+          throw new CorruptedJournalException(lastPosition, file, "corrupted READ_HEAD")
+        }
+        val id = b.getLong()
+        Record.ReadHead(id)
+      }
+      case PUT => {
+        if (b.limit < 20) {
+          throw new CorruptedJournalException(lastPosition, file, "corrupted PUT")
+        }
+        val id = b.getLong()
+        val addTime = b.getLong()
+        val expireTime = if (b.position + 4 <= b.limit) Some(b.getLong()) else None
+        Record.Put(QueueItem(id, Time.fromMilliseconds(addTime), expireTime.map { t =>
+          Time.fromMilliseconds(t)
+        }, data))
+      }
+      case READ_DONE => {
+        if (data.size % 8 != 0) {
+          throw new CorruptedJournalException(lastPosition, file, "corrupted READ_DONE")
+        }
+        val ids = new Array[Long](data.size / 8)
+        val b = ByteBuffer.wrap(data)
+        b.order(ByteOrder.LITTLE_ENDIAN)
+        for (i <- 0 until ids.size) { ids(i) = b.getLong() }
+        Record.ReadDone(ids)
+      }
+      case _ => throw new CorruptedJournalException(lastPosition, file, "unknown command " + command)
+    })
+  }
+}
---
+++ src/test/scala/com/twitter/libkestrel/JournalFileSpec.scala
@@ -0,0 +1,235 @@
+/*
+ * Copyright 2011 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.libkestrel
+
+import com.twitter.util._
+import java.io._
+import java.nio.ByteBuffer
+import org.specs.Specification
+
+class JournalFileSpec extends Specification with TempFolder {
+  def readFile(file: File) = {
+    val f = new FileInputStream(file).getChannel
+    val bytes = new Array[Byte](file.length.toInt)
+    val buffer = ByteBuffer.wrap(bytes)
+    var x = 0
+    do {
+      x = f.read(buffer)
+    } while (buffer.position < buffer.limit && x > 0)
+    f.close()
+    bytes
+  }
+
+  def writeFile(file: File, data: Array[Byte]) {
+    val f = new FileOutputStream(file).getChannel
+    val buffer = ByteBuffer.wrap(data)
+    do {
+      f.write(buffer)
+    } while (buffer.position < buffer.limit)
+    f.close()
+  }
+
+  def hex(bytes: Array[Byte]) = {
+    bytes.map { b => "%x".format(b) }.mkString(" ")
+  }
+
+  def unhex(s: String): Array[Byte] = {
+    s.split(" ").map { x => Integer.parseInt(x, 16).toByte }.toArray
+  }
+
+  "JournalFile" should {
+    "put" in {
+      val putData = "27 64 26 3 85 5 0 0 0 64 0 0 0 0 0 0 0 ff 0 0 0 0 0 0 0 68 65 6c 6c 6f"
+      val putItem = QueueItem(100, Time.fromMilliseconds(255), None, "hello".getBytes)
+
+      "write" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          val j = JournalFile.createWriter(testFile, null, Duration.MaxValue)
+          j.put(putItem)
+          j.close()
+          hex(readFile(testFile)) mustEqual putData
+        }
+      }
+
+      "read" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          writeFile(testFile, unhex(putData))
+          val j = JournalFile.openWriter(testFile, null, Duration.MaxValue)
+          j.readNext() mustEqual Some(JournalFile.Record.Put(putItem))
+        }
+      }
+
+      "read corrupted" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          val data = unhex(putData)
+          (0 until data.size).foreach { size =>
+            writeFile(testFile, data.slice(0, size))
+            if (size != 4) {
+              JournalFile.openWriter(testFile, null, Duration.MaxValue).readNext() must throwA[IOException]
+            }
+          }
+        }
+      }
+
+      "read several" in {
+        withTempFolder {
+          val headerData = "27 64 26 3"
+          val putData1 = "85 5 0 0 0 64 0 0 0 0 0 0 0 ff 0 0 0 0 0 0 0 68 65 6c 6c 6f"
+          val putItem1 = QueueItem(100, Time.fromMilliseconds(255), None, "hello".getBytes)
+          val putData2 = "85 7 0 0 0 65 0 0 0 0 0 0 0 84 3 0 0 0 0 0 0 67 6f 6f 64 62 79 65"
+          val putItem2 = QueueItem(101, Time.fromMilliseconds(900), None, "goodbye".getBytes)
+
+          val testFile = new File(folderName, "a1")
+          writeFile(testFile, unhex(headerData + " " + putData1 + " " + putData2))
+          val j = JournalFile.openWriter(testFile, null, Duration.MaxValue)
+          j.readNext() mustEqual Some(JournalFile.Record.Put(putItem1))
+          j.readNext() mustEqual Some(JournalFile.Record.Put(putItem2))
+          j.readNext() mustEqual None
+        }
+      }
+
+      "append" in {
+        withTempFolder {
+          val headerData = "27 64 26 3"
+          val putData1 = "85 5 0 0 0 64 0 0 0 0 0 0 0 ff 0 0 0 0 0 0 0 68 65 6c 6c 6f"
+          val putItem1 = QueueItem(100, Time.fromMilliseconds(255), None, "hello".getBytes)
+          val putData2 = "85 7 0 0 0 65 0 0 0 0 0 0 0 84 3 0 0 0 0 0 0 67 6f 6f 64 62 79 65"
+          val putItem2 = QueueItem(101, Time.fromMilliseconds(900), None, "goodbye".getBytes)
+
+          val testFile = new File(folderName, "a1")
+          val j = JournalFile.createWriter(testFile, null, Duration.MaxValue)
+          j.put(putItem1)
+          j.close()
+          val j2 = JournalFile.append(testFile, null, Duration.MaxValue)
+          j2.put(putItem2)
+          j2.close()
+
+          hex(readFile(testFile)) mustEqual headerData + " " + putData1 + " " + putData2
+        }
+      }
+    }
+
+    "readHead" in {
+      val readHeadData = "26 3c 26 3 2 ff 1 0 0 0 0 0 0"
+      val readHead = JournalFile.Record.ReadHead(511)
+
+      "write" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          val j = JournalFile.createReader(testFile, null, Duration.MaxValue)
+          j.readHead(511)
+          j.close()
+          hex(readFile(testFile)) mustEqual readHeadData
+        }
+      }
+
+      "read" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          writeFile(testFile, unhex(readHeadData))
+          val j = JournalFile.openReader(testFile, null, Duration.MaxValue)
+          j.readNext() mustEqual Some(readHead)
+        }
+      }
+
+      "read corrupted" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          val data = unhex(readHeadData)
+          (0 until data.size).foreach { size =>
+            writeFile(testFile, data.slice(0, size))
+            if (size != 4) {
+              JournalFile.openReader(testFile, null, Duration.MaxValue).readNext() must throwA[IOException]
+            }
+          }
+        }
+      }
+    }
+
+    "readDone" in {
+      val readDoneData = "26 3c 26 3 91 18 0 0 0 a 0 0 0 0 0 0 0 14 0 0 0 0 0 0 0 1e 0 0 0 0 0 0 0"
+      val readDone = JournalFile.Record.ReadDone(Array(10, 20, 30))
+
+      "write" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          val j = JournalFile.createReader(testFile, null, Duration.MaxValue)
+          j.readDone(List(10, 20, 30))
+          j.close()
+          hex(readFile(testFile)) mustEqual readDoneData
+        }
+      }
+
+      "read" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          writeFile(testFile, unhex(readDoneData))
+          val j = JournalFile.openReader(testFile, null, Duration.MaxValue)
+          j.readNext() mustEqual Some(readDone)
+        }
+      }
+
+      "read corrupted" in {
+        withTempFolder {
+          val testFile = new File(folderName, "a1")
+          val data = unhex(readDoneData)
+          (0 until data.size).foreach { size =>
+            writeFile(testFile, data.slice(0, size))
+            if (size != 4) {
+              JournalFile.openReader(testFile, null, Duration.MaxValue).readNext() must throwA[IOException]
+            }
+          }
+        }
+      }
+    }
+
+    "whole read file" in {
+      withTempFolder {
+        val fileData = "26 3c 26 3 91 8 0 0 0 2 0 1 0 0 0 0 0 2 0 0 1 0 0 0 0 0"
+        val testFile = new File(folderName, "a1")
+        writeFile(testFile, unhex(fileData))
+
+        val j = JournalFile.openReader(testFile, null, Duration.MaxValue)
+        j.readNext() mustEqual Some(JournalFile.Record.ReadDone(Array(65538)))
+        j.readNext() mustEqual Some(JournalFile.Record.ReadHead(65536))
+        j.readNext() mustEqual None
+      }
+    }
+
+    "refuse to deal with items too large" in {
+      withTempFolder {
+        val testFile = new File(folderName, "a1")
+        writeFile(testFile, unhex("27 64 26 3 85 ff ff ff 7f"))
+        
+        val j = JournalFile.openWriter(testFile, null, Duration.MaxValue)
+        j.readNext() must throwA[IOException].like {
+          case e: CorruptedJournalException => e.message == "item too large"
+        }
+        
+        val item = QueueItem(100, Time.fromMilliseconds(0), None,
+          new Array[Byte](JournalFile.LARGEST_DATA.inBytes.toInt + 1))
+        val j2 = JournalFile.createWriter(testFile, null, Duration.MaxValue)
+        j2.put(item) must throwA[IOException].like {
+          case e: IOException => e.getMessage == "item too large"
+        }
+      }
+    }
+  }
+}
