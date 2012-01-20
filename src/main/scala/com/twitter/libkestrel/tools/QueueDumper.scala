02ab888f04af61bda9b8a6d8b2ead4166db5ff35
Robey Pointer <robey@twitter.com>
2012-01-19 18:06:42 -0800

    fix position reporting.

--- src/main/scala/com/twitter/libkestrel/tools/QueueDumper.scala
+++ src/main/scala/com/twitter/libkestrel/tools/QueueDumper.scala
@@ -44,14 +44,16 @@ class QueueDumper(filename: String, quiet: Boolean, dump: Boolean, dumpRaw: Bool
       }
       var lastDisplay = 0L
 
+      var position = journalFile.position
       journalFile.foreach { record =>
         operations += 1
-        dumpItem(journalFile.position, record)
+        dumpItem(position, record)
         if (quiet && !dumpRaw && journalFile.position - lastDisplay > 1024 * 1024) {
           print("\rReading journal: %-6s".format(journalFile.position.bytes.toHuman))
           Console.flush()
           lastDisplay = journalFile.position
         }
+        position = journalFile.position
       }
       if (!dumpRaw) {
         print("\r" + (" " * 30) + "\r")
