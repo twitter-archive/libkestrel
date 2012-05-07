fc03500c5204d49d042afa9d00074e59d1da9e58
Robey Pointer <robey@twitter.com>
2012-05-07 16:58:16 -0700

    'release commit for com.twitter:libkestrel:1.0.3'

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object Libkestrel extends Build {
   ).settings(
     name := "libkestrel",
     organization := "com.twitter",
-    version := "1.0.3-SNAPSHOT",
+    version := "1.0.3",
     scalaVersion := "2.8.1",
 
     // time-based tests cannot be run in parallel
