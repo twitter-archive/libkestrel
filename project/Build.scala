6b06438c8eb2d048b769cada7cb65c10dcbc8153
Stephan Zuercher <stephan@twitter.com>
2012-05-15 10:48:39 -0700

    'release commit for com.twitter:libkestrel:1.0.4'

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,7 @@ object Libkestrel extends Build {
   ).settings(
     name := "libkestrel",
     organization := "com.twitter",
-    version := "1.0.4-SNAPSHOT",
+    version := "1.0.4",
     scalaVersion := "2.8.1",
 
     // time-based tests cannot be run in parallel
